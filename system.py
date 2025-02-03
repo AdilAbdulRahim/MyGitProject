e8317772a4d53e603e6c9bd0d30059e2977173bd
eh7saan <157976490+eh7saan@users.noreply.github.com>
2025-02-03 18:15:39 -0400

    Update system.py

--- system.py
+++ system.py
@@ -1,8 +1,8 @@
-#validation sys function 
-highScore = None
-def validateInput(userInput):
+high_score = None
+#validate
+def validate_input(user_input):
     try:
-        guess = int(userInput)
+        guess = int(user_input)
         if 1 <= guess <= 100:
             return guess
         else:
@@ -11,12 +11,11 @@ def validateInput(userInput):
     except ValueError:
         print("Invalid input. Please enter a valid number.")
         return None
-
-#scoring system function
-def updateScoringSystem(attempts):
-    global highScore
-    if highScore is None or attempts < highScore:
-        highScore = attempts
-        print(f"New high score! You set the record with {highScore} attempts.")
+#update score
+def update_scoring_system(attempts):
+    global high_score
+    if high_score is None or attempts < high_score:
+        high_score = attempts
+        print(f"New high score! You set the record with {high_score} attempts.")
     else:
-        print(f"Current high score is {highScore} attempts.")
+        print(f"Current high score is {high_score} attempts.")
